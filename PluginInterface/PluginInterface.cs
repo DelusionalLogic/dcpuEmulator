fa5eadba45971f396d803791ec1581c22575292d
Jesper Jensen <DelusionalLogic@gmail.com>
2012-11-08 22:41:48 +0100

    Fixed address datatype Added Ram interface Added screen interface

--- PluginInterface/PluginInterface.cs
+++ PluginInterface/PluginInterface.cs
@@ -8,13 +8,24 @@ namespace PluginInterface
 {
     public interface IPluginHost
     {
-        ushort readMem(ushort address);
+        ushort readMem(int address);
         void writeMem(int address, ushort value);
     }
 
     public interface ICpu : IPlugin
     {
         void tick();
+    }
+
+    public interface IRam : IPlugin
+    {
+        ushort readMem(int address);
+        void writeMem(int address, ushort value);
+    }
+
+    public interface IScreen : IPlugin
+    {
+        
     }
 
     public interface IPlugin
