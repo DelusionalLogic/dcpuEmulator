dbdb8d541234a8adee37d87c3a86a86d87ccf2e4
Jesper Jensen <DelusionalLogic@gmail.com>
2012-11-17 01:33:10 +0100

    Started KeyboardClass globalKeyboardHook is from codeProject.com: http://www.codeproject.com/Articles/19004/A-Simple-C-Global-Low-Level-Keyboard-Hook

---
+++ Generic Keyboard/Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginInterface;
+
+namespace Generic_Keyboard
+{
+    public class Main : IHardware
+    {
+        public IPluginHost Host { get; set; }
+
+        public bool configPossible { get { return false; } }
+
+        public void openConfig()
+        {
+        }
+
+        public void initialize()
+        {
+        }
+
+        public ushort[] interrupt(ushort[] registers)
+        {
+        }
+
+        public void dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public string Name
+        {
+            get { return "Generic Keyboard"; }
+        }
+
+        public string Description
+        {
+            get { return "Generic Keyboard"; }
+        }
+
+        public string Author
+        {
+            get { return "DelusionalLogic"; }
+        }
+
+        public string Version
+        {
+            get { return "1.0"; }
+        }
+
+        public uint ID { get { return 0x30cf7406; } }
+        public ushort HVersion { get { return 1; } }
+        public uint ManufacturerID { get { return 0x0; } }
+    }
+}
